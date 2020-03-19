745f37405ab4502b8489041af5d3583675280fab
Taras Tarlov <ttarlov@gmail.com>
2020-03-19 08:23:59 -0600

    Add beforeEach Hook.

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,6 +6,14 @@ const Turn = require('../src/Turn');
 
 
 describe('Turn', function() {
+    let card;
+    let turn;
+
+  beforeEach(function(){
+    card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    turn = new Turn('object', card)
+  });
+
 
 
   it('should be a function', function() {
@@ -19,29 +27,23 @@ describe('Turn', function() {
   });
 
   it('Should initilize with a guess.', function(){
-    let card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-    let turn = new Turn('object', card)
     expect(turn.guess).to.equal('object');
   });
 
   it('Should initilize with a current card at play.', function(){
-    let card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-    let turn = new Turn('object', card)
     expect(turn.currentCard).to.equal(card);
   });
 
   describe('returnGuess Method', function(){
+
     it('Should return players Guess', function(){
-      let card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-      let turn = new Turn('object', card)
       expect(turn.returnGuess()).to.equal('object')
     });
   });
 
   describe('returnCard Method', function(){
+
     it('Should return current card at play', function(){
-      let card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-      let turn = new Turn('object', card)
       expect(turn.returnCard()).to.equal(card)
     });
 
@@ -49,36 +51,29 @@ describe('Turn', function() {
   });
 
   describe('evaluateGuess Method', function(){
+
     it('Should return TRUE if the guess matches the correct answer', function(){
-      let card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-      let turn = new Turn('object', card)
       expect(turn.evaluateGuess()).to.equal(true)
     });
 
     it('Should return FALSE if the guess DOES NOT match the correct answer', function(){
-      let card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-      let turn = new Turn('array', card)
+      turn = new Turn('array', card)
       expect(turn.evaluateGuess()).to.equal(false)
     });
   });
 
 
   describe('giveFeedback Method', function(){
+
     it('should provide positive feedback if correct answer is given', function(){
-      let card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-      let turn = new Turn('object', card)
       expect(turn.giveFeedback()).to.equal("Correct, Great Job!");
     });
 
     it('should provide negative feedback if incorrect answer is given', function(){
-      let card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
-      let turn = new Turn('array', card)
+      turn = new Turn('array', card)
       expect(turn.giveFeedback()).to.equal("Wrong, try again!");
     });
 
   });
-
-
-
-
+  
 });
