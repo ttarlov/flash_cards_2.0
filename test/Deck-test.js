ea27da88eba4014695e6ca02e0390032f37eb405
Taras Tarlov <ttarlov@gmail.com>
2020-03-19 08:22:57 -0600

    setup beforeEach hook and correct grammer

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,6 +6,18 @@ const Card = require('../src/Card');
 
 
 describe('Deck', function() {
+  let deck;
+  let card1;
+  let card2;
+  let card3;
+
+  beforeEach(function(){
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3]);
+  });
+
 
   it('should be a function', function() {
     const deck = new Deck();
@@ -18,26 +30,15 @@ describe('Deck', function() {
   });
 
   it('should initialize with an array of Card objects', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3]);
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
   describe('countCards Method', function(){
 
     it('should be able to count cards', function(){
-      const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-      const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-      const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-      const deck = new Deck([card1, card2, card3]);
       expect(deck.countCards()).to.equal(3);
     });
 
-
   });
-
-
 
 });
