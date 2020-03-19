533b4f2e63badba91c53bc7420ff4dcdbc9e5b74
Taras Tarlov <ttarlov@gmail.com>
2020-03-18 19:57:37 -0600

    add incorrectGuesses array, add takeTurn method

--- src/Round.js
+++ src/Round.js
@@ -1,17 +1,26 @@
+
+const Turn = require('../src/Turn')
+
 class Round {
   constructor(deck) {
     this.deck = deck.cards;
     this.turnCount = 0;
+    this.incorrectGuesses = [];
   }
 
   returnCurrentCard() {
     return this.deck.shift()
   }
 
-  takeTurn() {
+  takeTurn(guess) {
+    let turn = new Turn(guess, this.returnCurrentCard())
+    if(turn.evaluateGuess() === false) {
+      this.incorrectGuesses.push(turn.currentCard.id);
+    }
     this.turnCount++
-    this.returnCurrentCard()
+    turn.giveFeedback();
   }
+
 
 
 }
