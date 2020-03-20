1b39c855d1e2654bde8f077210dc8db4f07c2ae2
Taras Tarlov <ttarlov@gmail.com>
2020-03-20 08:38:35 -0600

    Correct ESLint errors.

--- test/Round-test.js
+++ test/Round-test.js
@@ -14,7 +14,7 @@ describe('Round', function() {
   let deck;
   let turn;
 
-  beforeEach(function(){
+  beforeEach(function() {
     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
@@ -36,31 +36,31 @@ describe('Round', function() {
     expect(round.deck).to.deep.equal([card1, card2, card3])
   })
 
-  it('Should start with no incorrect guesses', function(){
+  it('Should start with no incorrect guesses', function() {
     expect(round.incorrectGuesses).to.deep.equal([]);
   });
 
-  it('should start with timer at zero', function(){
+  it('should start with timer at zero', function() {
     expect(round.timer).to.equal(0)
   });
 
-  describe('returnCurrentCard Method', function(){
-    it('Should return the first card in the deck', function(){
+  describe('returnCurrentCard Method', function() {
+    it('Should return the first card in the deck', function() {
       expect(round.returnCurrentCard()).to.equal(card1)
     });
   });
 
-  describe('takeTurn Method', function(){
-    it('should start with turnCount property at zero', function(){
+  describe('takeTurn Method', function() {
+    it('should start with turnCount property at zero', function() {
       expect(round.turnCount).to.equal(0);
     });
 
-    it('should increment turnCount by 1 after taking a turn', function(){
+    it('should increment turnCount by 1 after taking a turn', function() {
       round.takeTurn();
       expect(round.turnCount).to.equal(1);
     });
 
-    it('should jump to the next card in deck', function(){
+    it('should jump to the next card in deck', function() {
       expect(round.deck[round.turnCount]).to.equal(card1);
       round.takeTurn();
       expect(round.deck[round.turnCount]).to.equal(card2);
@@ -68,50 +68,45 @@ describe('Round', function() {
       expect(round.deck[round.turnCount]).to.equal(card3);
     });
 
-    it('should store incorrect guess via ID', function(){
+    it('should store incorrect guess via ID', function() {
       turn = new Turn('pug', card1);
       round.takeTurn('pug');
       expect(round.incorrectGuesses).to.deep.equal([1])
-      turn2 = new Turn('appendix', card2);
       round.takeTurn('appendix');
-      expect(round.incorrectGuesses).to.deep.equal([1,14])
+      expect(round.incorrectGuesses).to.deep.equal([1, 14])
     });
 
-    it('Should say "Correct, Great Job" if question is answered correctly' , function(){
+    it('Should say "Correct, Great Job" if question is answered correctly', function() {
       turn = new Turn('sea otter', card1);
       round.takeTurn('sea otter');
-      expect(turn.giveFeedback()).to.equal('Correct, Great Job!')
+      expect(turn.giveFeedback()).to.equal(" ◕‿◕ Correct, Great Job!")
     });
 
-    it('Should say "Wrong, try again!" if question is answered incorrectly', function(){
+    it('Should say "Wrong, try again!" if question is answered incorrectly', function() {
       turn = new Turn('pug', card1);
       round.takeTurn('pug');
-      expect(turn.giveFeedback()).to.equal("Wrong, try again!")
+      expect(turn.giveFeedback()).to.equal(" ಠ_ಠ Wrong, try again!")
     });
   });
 
-  describe('calculatePercentCorrect Method', function(){
+  describe('calculatePercentCorrect Method', function() {
 
-    it('Should calculate and return the percentage of correct guesses', function(){
+    it('Should calculate and return the percentage of correct guesses', function() {
       turn = new Turn('sea otter', card1);
       round.takeTurn('sea otter');
-      turn2 = new Turn('appendix', card2);
       round.takeTurn('appendix');
-      turn3 = new Turn('Fitzgerald', card3)
       round.takeTurn('Fitzgerald');
       expect(round.calculatePercentCorrect()).to.equal(66)
     });
   });
 
 
-  describe('endRound Method', function(){
+  describe('endRound Method', function() {
 
-    it('should print a message after round ends with percentage of correctly answered questions', function(){
+    it('should print a message after round ends with percentage of correctly answered questions', function() {
       turn = new Turn('sea otter', card1);
       round.takeTurn('sea otter');
-      turn2 = new Turn('appendix', card2);
       round.takeTurn('appendix');
-      turn3 = new Turn('Fitzgerald', card3)
       round.takeTurn('Fitzgerald');
       expect(round.endRound()).to.equal("** Round over! ** You answered 66% of the questions correctly!")
     });
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,10 +6,10 @@ const Turn = require('../src/Turn');
 
 
 describe('Turn', function() {
-    let card;
-    let turn;
+  let card;
+  let turn;
 
-  beforeEach(function(){
+  beforeEach(function() {
     card = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
     turn = new Turn('object', card)
   });
@@ -17,59 +17,59 @@ describe('Turn', function() {
 
 
   it('should be a function', function() {
-    let turn = new Turn();
+    // let turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', function() {
-    let turn = new Turn();
+    // let turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it('Should initilize with a guess.', function(){
+  it('Should initilize with a guess.', function() {
     expect(turn.guess).to.equal('object');
   });
 
-  it('Should initilize with a current card at play.', function(){
+  it('Should initilize with a current card at play.', function() {
     expect(turn.currentCard).to.equal(card);
   });
 
-  describe('returnGuess Method', function(){
+  describe('returnGuess Method', function() {
 
-    it('Should return players Guess', function(){
+    it('Should return players Guess', function() {
       expect(turn.returnGuess()).to.equal('object')
     });
   });
 
-  describe('returnCard Method', function(){
+  describe('returnCard Method', function() {
 
-    it('Should return current card at play', function(){
+    it('Should return current card at play', function() {
       expect(turn.returnCard()).to.equal(card)
     });
 
 
   });
 
-  describe('evaluateGuess Method', function(){
+  describe('evaluateGuess Method', function() {
 
-    it('Should return TRUE if the guess matches the correct answer', function(){
+    it('Should return TRUE if the guess matches the correct answer', function() {
       expect(turn.evaluateGuess()).to.equal(true)
     });
 
-    it('Should return FALSE if the guess DOES NOT match the correct answer', function(){
+    it('Should return FALSE if the guess DOES NOT match the correct answer', function() {
       turn = new Turn('array', card)
       expect(turn.evaluateGuess()).to.equal(false)
     });
   });
 
 
-  describe('giveFeedback Method', function(){
+  describe('giveFeedback Method', function() {
 
-    it('should provide positive feedback if correct answer is given', function(){
+    it('should provide positive feedback if correct answer is given', function() {
       expect(turn.giveFeedback()).to.equal(" ◕‿◕ Correct, Great Job!");
     });
 
-    it('should provide negative feedback if incorrect answer is given', function(){
+    it('should provide negative feedback if incorrect answer is given', function() {
       turn = new Turn('array', card)
       expect(turn.giveFeedback()).to.equal(" ಠ_ಠ Wrong, try again!");
     });
